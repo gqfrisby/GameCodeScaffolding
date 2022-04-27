410eed6af412304ad879dc5f3bc676afece707a7
brewerkm1 <101234022+brewerkm1@users.noreply.github.com>
2022-04-27 19:08:18 -0400

    Update NPC.java

--- NPC.java
+++ NPC.java
@@ -10,17 +10,40 @@ public class NPC
 
     public static void goodNPCInteraction(){
         int input;
-        System.out.println("Villager approaches! Villager says: Hi, traveller.");
+        System.out.println("Child villager approaches! Villager says: Hi, traveller.");
         System.out.println("\n1. What can you tell me about this area?" +
-                "\n2. Who controls this area?" +
+                "\n2. Hello." +
                 "\n3. [Ignore]");
         input = kb.nextInt();
         if(input == 1){
-            System.out.println("");
+            System.out.println("Villager: We are a very poor town indeed. My mother had to get another job last week!");
+            System.out.println("\n1. I'm sorry to hear that." +
+                    "\n2. I'm going to help you.");
+            input = kb.nextInt();
+                if(input == 1){
+                    System.out.println("Villager: I wish someone would save us.");
+                    System.out.println("(The villager left, downtrodden.)");
+                }
+                if(input == 2){
+                    System.out.println("Villager: Really? Well, then you'll need my help, too!");
+                    System.out.println("(The villager hands you something. You're not exactly sure what it's supposed to do.");
+                    System.out.println("(The villager leaves, skipping.)");
+                }
         }
         if(input == 2){
 
-            System.out.println("");
+            System.out.println("Villager: Are you the one the moon spoke of?");
+            System.out.println("\n1. Yes, of course." +
+                    "\n2. I guess...");
+            input = kb.nextInt();
+                if(input == 1){
+                    System.out.println("Villager: Good!");
+                    System.out.println("(The villager left, excited.)");
+                }
+                if(input == 2){
+                    System.out.println("You don't sound too confident. I wish they sent someone else.");
+                    System.out.println("(The villager left, worried.)");
+                }
         }
         if(input == 3){
             System.out.println("Villager: All right, then. See you, I guess.");
@@ -39,8 +62,8 @@ public class NPC
                 3. [Ignore]""");
         input = kb.nextInt();
         if(input == 1){
-            System.out.println("(You lost 1 coin. Money: " + currentCurrency + ")");
             currentCurrency = currentCurrency -1;
+            System.out.println("(You lost 1 coin. Money: " + currentCurrency + ")");
 
             System.out.println("Villager: That's funny. Got any more?");
             System.out.println("""
@@ -55,8 +78,8 @@ public class NPC
 
             }
             if(input == 2){
-                System.out.println("(You lost 1 coin. Money: " + currentCurrency + ")");
                 currentCurrency = currentCurrency -1;
+                System.out.println("(You lost 1 coin. Money: " + currentCurrency + ")");
                 System.out.println("Villager: Yeah, all right. Very, VERY funny.");
                 System.out.println("The villager left, grumbling.");
             }
